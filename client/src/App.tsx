0aaecaf702f275edb502a82eed02a19c8f4c1e12
Bruno Fernandez <bruno.fernandez@oclock.school>
2026-03-16 14:14:17 +0100

    feat(client): add cgv component and add on LandingPage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,22 @@
-// import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/header';
 import LandingPage from './components/landingPage.tsx';
 import Footer from './components/footer.tsx';
+import PrivacyPolicy from './components/privacyPolicy.tsx';
+import CGV from './components/cgv.tsx';
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
       <Header />
-      <LandingPage />
-      <Footer/>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/cgv" element={<CGV />} />
+      </Routes>
+      <Footer />
     </>
   )
 
