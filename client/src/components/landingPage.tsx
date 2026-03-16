59bb812a2d02e16aeeffd06825a7025ce34c983c
Bruno Fernandez <bruno.fernandez@oclock.school>
2026-03-16 10:07:52 +0100

    chore(client): add react-router-dom + setup BrowserRouter in main

---
+++ client/src/components/landingPage.tsx
@@ -0,0 +1,12 @@
+<header>
+    <img src="/logo" alt="logo entreprise" />
+    <p className="name_enterprise">GreenRoots</p>
+    <nav>
+    <a href="/landing">Accueil</a>
+    <a href="/catalog">Catalogu</a>
+    <a href="/about">A propos</a>
+    <a href="/contact">Contact</a>
+    </nav>
+    <button className="register">Inscription</button>
+    <button className="login">Connexion</button>
+</header>
