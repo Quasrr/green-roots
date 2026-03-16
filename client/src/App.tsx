ce6a0720198b6c823085112c8d4d0caa3bae3562
Bruno Fernandez <bruno.fernandez@oclock.school>
2026-03-16 10:32:00 +0100

    fix(client): landingPage component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
-// import { useState } from 'react'
+// import { Routes, Route } from 'react-router-dom';
+import Header from './components/header';
 import './App.css'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-
+      <Header/>
   )
 
 }
