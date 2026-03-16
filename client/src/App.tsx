2846bcc93cabb8aef97eade71a8cc2d0dd3e2713
Bruno Fernandez <bruno.fernandez@oclock.school>
2026-03-16 13:05:23 +0100

    feat(client): add Routes and route for LandingPage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,16 @@
 // import { Routes, Route } from 'react-router-dom';
 import Header from './components/header';
+import LandingPage from './components/landingPage.tsx';
 import './App.css'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-      <Header/>
+    <>
+      <Header />
+      <LandingPage />
+    </>
   )
 
 }
