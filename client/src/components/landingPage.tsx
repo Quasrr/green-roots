2eaab7f72536eafb3012d6bc7291c1b471f0b3fc
Bruno Fernandez <bruno.fernandez@oclock.school>
2026-03-16 10:32:00 +0100

    fix(client): landingPage component

--- client/src/components/landingPage.tsx
+++ client/src/components/landingPage.tsx
@@ -1,12 +1,1 @@
-<header>
-    <img src="/logo" alt="logo entreprise" />
-    <p className="name_enterprise">GreenRoots</p>
-    <nav>
-    <a href="/landing">Accueil</a>
-    <a href="/catalog">Catalogu</a>
-    <a href="/about">A propos</a>
-    <a href="/contact">Contact</a>
-    </nav>
-    <button className="register">Inscription</button>
-    <button className="login">Connexion</button>
-</header>
+// import { Link } from 'react-router-dom';
