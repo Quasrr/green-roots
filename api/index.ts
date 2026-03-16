8f273fccccd702a8f6ec7d76ff5e82025c3d013c
TonyLcfOclock <tony.lecerf@oclock.school>
2026-03-16 15:02:39 +0100

    feat(api): Routes register and login added

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import router from './src/routes.ts';
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
+
+app.use(express.json());
+
+app.use(router);
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
