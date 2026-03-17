4a092918939b993da739b2478041c0812f493db6
Bruno Fernandez <bruno.fernandez@oclock.school>
2026-03-17 15:20:01 +0100

    feat(css): Add Css for Header

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,22 +1,27 @@
 import { Link } from 'react-router-dom';
+import '../Header.css'
 
 function Header() {
     return (
         <header>
-            <img src="/logo.webp" alt="Logo GreenRoots" />
+            <div className='div_logo_header'>
+            <img src="/logo_greenroots.webp" alt="Logo GreenRoots" className='logo_greenroots'/>
             <p className="name_enterprise">GreenRoots</p>
+            </div>
             <nav>
                 <Link to="/">Accueil</Link>
                 <Link to="/catalog">Catalogue</Link>
                 <Link to="/about">A propos</Link>
                 <Link to="/contact">Contact</Link>
             </nav>
+            <div className='div_buttons_header'>
             <Link to="/register">
                 <button className="register">Inscription</button>
             </Link>
             <Link to="/login">
                 <button className="login">Connexion</button>
             </Link>
+            </div>
         </header>
     )
 }
