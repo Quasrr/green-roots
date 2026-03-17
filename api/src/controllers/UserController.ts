9baed66e09ea630e08fb733bb91170f152bd3297
Mani-Ho <seb.gobbi@gmail.com>
2026-03-17 13:59:52 +0100

    feat(api): add UserController + routes

---
+++ api/src/controllers/UserController.ts
@@ -0,0 +1,66 @@
+import type { Request, Response } from "express";
+import { prisma } from '../models/index.ts';
+import { NotFoundError } from "../utils/Error.ts";
+
+
+const userSelect = {
+    id: true,
+    address: true,
+    email: true,
+    firstname: true,
+    lastname: true,
+    role: {
+        select: { nameRole: true }
+    }
+};
+
+class UserController {
+
+    async getAll(req: Request, res: Response) {
+        const users = await prisma.user.findMany({
+            select: userSelect
+        });
+        res.json(users);
+    }
+
+    async getById(req: Request, res: Response) {
+        const { id } = req.params;
+        const user = await prisma.user.findUnique({
+            where: { id: Number(id) },
+            select: userSelect
+        });
+        if (!user) {
+            throw new NotFoundError('User not found');
+        }
+        res.json(user);
+    }
+    async update(req: Request, res: Response) {
+        const { id } = req.params;
+        const data = req.body;
+
+        try {
+            const user = await prisma.user.update({
+                where: { id: Number(id) },
+                data: data,
+                select: userSelect
+            });
+            res.json(user);
+        } catch {
+            throw new NotFoundError('User not found');
+        }
+
+    }
+
+    async delete(req: Request, res: Response) {
+        const { id } = req.params;
+        try {
+            await prisma.user.delete({
+                where: { id: Number(id) }
+            });
+            res.status(204).send();
+        } catch {
+            throw new NotFoundError('User not found');
+        }
+    }
+}
+export default new UserController();
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import AuthController from "./controllers/AuthController.ts";
 import authMiddleware from "./Middlewares/authMiddleware.ts";
+import UserController from "./controllers/UserController.ts";
+import adminMiddleware from "./Middlewares/adminMiddleware.ts";
 const router = Router();
 
 // routes d'authentification
@@ -8,5 +10,11 @@ router.post('/api/auth/register', AuthController.register);
 router.post('/api/auth/login', AuthController.login);
 router.post('/api/auth/logout', authMiddleware, AuthController.logout);
 router.get('/api/auth/me', authMiddleware, AuthController.me);
+
+// routes utilisateur
+router.get('/api/users', authMiddleware, adminMiddleware, UserController.getAll);
+router.get('/api/users/:id', authMiddleware, UserController.getById);
+router.patch('/api/users/:id', authMiddleware, UserController.update);
+router.delete('/api/users/:id', authMiddleware, UserController.delete);
 
 export default router;
