5d942428cb8d17712af7666f07f6b787d10b294c
Mani-Ho <seb.gobbi@gmail.com>
2026-03-18 15:00:44 +0100

    feat(api): add getMyOrders

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -30,7 +30,7 @@ router.delete('/api/trees/:id', authMiddleware, adminMiddleware, TreesController
 // routes Commandes
 router.get('/api/orders', authMiddleware, adminMiddleware, OrdersController.getOrders);
 router.post('/api/orders', authMiddleware, OrdersController.createOrder);
-
+router.get('/api/orders/me', authMiddleware, OrdersController.getMyOrders);
 
 
 export default router;
