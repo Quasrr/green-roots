cb131a5700d5537594d6cd6ced22656e8c2c1816
TonyLcfOclock <tony.lecerf@oclock.school>
2026-03-19 18:43:51 +0100

    style(client): Add padding + horizontal bar in header

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -27,7 +27,7 @@ function Register() {
             const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/register`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ firstname, lastname, email, password, passwordConfirm: confirmPassword }),
+                body: JSON.stringify({ firstname, lastname, email, password }),
             });
 
             if (!res.ok) {
